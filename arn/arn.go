5b5652513003afd4c2d7263268ff2635f163818e
Jonathan Turner <jt@jtnet.co.uk>
2018-01-01 19:05:49 +0000

    updated with aws Paris region

--- arn/arn.go
+++ arn/arn.go
@@ -14,19 +14,20 @@ import (
 //arn:partition:service:region:account-id:resourcetype:resource
 
 var Regions = []string{
-	"us-east-2",
 	"us-east-1",
+	"us-east-2",
 	"us-west-1",
 	"us-west-2",
 	"ca-central-1",
 	"ap-south-1",
+	"ap-northeast-1",
 	"ap-northeast-2",
 	"ap-southeast-1",
 	"ap-southeast-2",
-	"ap-northeast-1",
 	"eu-central-1",
 	"eu-west-1",
 	"eu-west-2",
+	"eu-west-3",
 	"sa-east-1",
 }
 
